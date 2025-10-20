f3b8b9738a5b2515aff7117c841284821408981d
raimis001 <raimis0001@gmail.com>
2025-10-20 17:27:03 +0300

    Restore deleted file

---
+++ Assets/Scripts/SaveSystem/SaveManager.cs
@@ -0,0 +1,69 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+namespace AOGSaveSystem
+{
+
+    public partial struct SaveClass
+    {
+
+    }
+
+    static public class SaveManager
+    {
+        public static SaveClass SaveData;
+        public static string SaveFileName = "savefile.sav";
+
+        public static Action BeforeSave;
+        public static Action AfterSave;
+        public static Action BeforeLoad;
+        public static Action AfterLoad;
+
+
+        //static string saveFilePath => Path.Combine(Application.persistentDataPath, SaveFileName);
+        static string saveFilePath => Path.Combine(Application.streamingAssetsPath, SaveFileName);
+
+        public static async void Save() => await SaveTask();
+        public static bool Load() => LoadTask();
+
+        private static async Task SaveTask()
+        {
+            BeforeSave?.Invoke();
+
+            string saveText = GetSaveText();
+            await File.WriteAllTextAsync(saveFilePath, saveText);
+
+            AfterSave?.Invoke();
+        }
+
+        private static bool LoadTask()
+        {
+            if (!File.Exists(saveFilePath))
+            {
+                Debug.LogWarning($"Save file not found at {saveFilePath}");
+                return false;
+            }
+
+            BeforeLoad?.Invoke();
+
+            string stringData = File.ReadAllText(saveFilePath);
+            SetSaveData(stringData);
+
+            AfterLoad?.Invoke();
+            return true;
+        }
+
+        private static string GetSaveText()
+        {
+            return JsonUtility.ToJson(SaveData);
+        }
+
+        private static void SetSaveData(string stringData)
+        {
+            SaveData = JsonUtility.FromJson<SaveClass>(stringData);
+        }
+    }
+}
